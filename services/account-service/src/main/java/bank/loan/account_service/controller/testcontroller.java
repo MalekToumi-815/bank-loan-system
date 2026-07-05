4c15d70390743247e6078772cbc5130be9e9ce6c
MalekToumi-815 <191920560+MalekToumi-815@users.noreply.github.com>
2026-07-05 13:57:33 +0100

    created endpoints to test spring security later

---
+++ services/account-service/src/main/java/bank/loan/account_service/controller/testcontroller.java
@@ -0,0 +1,16 @@
+package bank.loan.account_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/test")
+public class testcontroller {
+    @GetMapping("")
+    public String getMethodName() {
+        return new String("Hello World");
+    }
+    
+}
