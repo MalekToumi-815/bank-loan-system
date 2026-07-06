ea5cf6aa75179d17ca46b3df617903be8339a280
MalekToumi-815 <191920560+MalekToumi-815@users.noreply.github.com>
2026-07-06 14:13:49 +0100

    Add User entity and JPA dependency for account service

---
+++ services/account-service/src/main/java/bank/loan/account_service/model/User.java
@@ -0,0 +1,88 @@
+package bank.loan.account_service.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String cin;
+    private String phone;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+
+    public User() {}
+
+    public User(String name, String surname, String cin, String phone, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.cin = cin;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
